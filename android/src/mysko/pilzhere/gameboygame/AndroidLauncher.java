eca9873f122c76a4eb44d8215c6c61981601ddb7
Unknown <svampodling@gmail.com>
2018-04-08 20:56:14 +0200

    Project created.

---
+++ android/src/mysko/pilzhere/gameboygame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package mysko.pilzhere.gameboygame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import mysko.pilzhere.gameboygame.GameboyGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameboyGame(), config);
+	}
+}
---
+++ desktop/src/mysko/pilzhere/gameboygame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package mysko.pilzhere.gameboygame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import mysko.pilzhere.gameboygame.GameboyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameboyGame(), config);
+	}
+}
